90ac6ce4c7024adab316128ab5cf08ace6afb29f
AsiriM2000 <asiriambagahawaththa@gmail.com>
2023-04-01 10:02:02 +0530

    validator form implement

--- src/Pages/Customer/index.jsx
+++ src/Pages/Customer/index.jsx
@@ -1,12 +1,125 @@
-import { Component } from "react";
+import { Component,Fragment } from 'react';
+import Grid from '@mui/material/Grid';
+import ButtonCommon from "../../Component/Common/Button";
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Typography } from '@mui/material';
 
-class Customer extends Component{
-    render(){
-        return(
-            <div>
-                <h1>This Is Customer Page</h1>
-            </div>
+class Customer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formData: {
+                userId: '',
+                id: '',
+                title: '',
+                body: ''
+            }
+        }
+
+    }
+
+    handleSubmit = () => {
+        console.log('save button clicked!!')
+        console.log(this.state.formData)
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Fragment>
+                <Typography variant="h4">
+                    Customer Manage
+                </Typography>
+                <ValidatorForm
+                    ref="form"
+                    onSubmit={this.handleSubmit}
+                    onError={errors => console.log(errors)}
+                >
+                    <Grid container spacing={0.5}>
+                        <Grid item lg={6} md={6} sm={6} xm={6} >
+                            <Typography variant="body2">User Id</Typography>
+                            <TextValidator
+                                id="outlined-basic"
+                                placeHolder="User Id"
+                                variant="outlined"
+                                size="small"
+                                value={this.state.formData.userId}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.userId = e.target.value
+                                    this.setState({formData})
+                                }}
+                                style={{ width: '100%' }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={6} xm={6}>
+                            <Typography variant="body2">Id</Typography>
+                            <TextValidator
+                                id="outlined-basic"
+                                placeHolder="Id"
+                                variant="outlined"
+                                size="small"
+                                style={{ width: '100%' }}
+                                validators={['required']}
+                                value={this.state.formData.id}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.id = e.target.value
+                                    this.setState({formData})
+                                }}
+                            />
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={6} xm={6} >
+                            <Typography variant="body2">Title</Typography>
+                            <TextValidator
+                                id="outlined-basic"
+                                placeHolder="Title"
+                                variant="outlined"
+                                size="small"
+                                value={this.state.formData.title}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.title = e.target.value
+                                    this.setState({formData})
+                                }}
+                                style={{ width: '100%' }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={6} xm={6}>
+                            <Typography variant="body2">Body</Typography>
+                            <TextValidator
+                                id="outlined-basic"
+                                placeHolder="Body"
+                                variant="outlined"
+                                size="small"
+                                value={this.state.formData.body}
+                                onChange={(e) => {
+                                    let formData = this.state.formData
+                                    formData.body = e.target.value
+                                    this.setState({formData})
+                                }}
+                                style={{ width: '100%' }}
+                                validators={['required']}
+                            />
+                        </Grid>
+                        <Grid item lg={12} md={12} sm={12} xm={12} style={{ display: 'flex' }} justifyContent="flex-end" >
+                            <ButtonCommon 
+                                variant="contained" 
+                                label="save" 
+                                type="submit" 
+                                onClick={()=>{
+                                    this.handleSubmit()
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </ValidatorForm>
+            </Fragment>
+
         )
     }
 }
+
 export default Customer;
